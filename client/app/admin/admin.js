e7ce9fd1f25eb11076e2baab531a07342d48079d
pingjiang <pingjiang1989@gmail.com>
2014-09-06 08:54:39 +0800

    Initial Commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testmeApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
